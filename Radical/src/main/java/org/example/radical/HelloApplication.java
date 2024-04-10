cb487e354bd5ad2d7d4a486bfa005a0fc67c8620
JazzyMrDerp <jamehta19@gmail.com>
2024-04-10 14:30:51 -0400

    Did rectangle

--- Radical/src/main/java/org/example/radical/HelloApplication.java
+++ Radical/src/main/java/org/example/radical/HelloApplication.java
@@ -2,7 +2,11 @@ package org.example.radical;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +14,19 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Group root = new Group();
+        stage.setTitle("Color Example");
+        Rectangle rect = new Rectangle();
+        rect.setX(50);
+        rect.setY(20);
+        rect.setWidth(200);
+        rect.setHeight(250);
+        rect.setEffect(new DropShadow());
+        root.getChildren().add(rect);
+        Scene scene = new Scene(root,300,400, Color.hsb(180, 1, 1));
         stage.setScene(scene);
         stage.show();
+
     }
 
     public static void main(String[] args) {
